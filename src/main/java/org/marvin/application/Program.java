88fecd92cbba4b505828d6c1da0a1fa1fc8d8d78
Afrânio Nunes Dantas <afraniodantas0224@gmail.com>
2025-05-27 22:26:19 -0300

    feat: add method area for delegate responsability

--- src/main/java/org/marvin/application/Program.java
+++ src/main/java/org/marvin/application/Program.java
@@ -24,11 +24,9 @@ public class Program {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+        double areaX = x.area();
 
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        double areaY = y.area();
 
         System.out.printf("Triangle X Area: %.4f%n", areaX);
         System.out.printf("Triangle Y Area: %.4f%n", areaY);
@@ -37,6 +35,5 @@ public class Program {
 
         System.out.println("Large area: " + largeArea);
         sc.close();
-
     }
 }
